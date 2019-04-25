de037a6eac12f4d45459523d5d4945066fe989c0
1435424666 <48304304+1435424666@users.noreply.github.com>
2019-04-25 18:58:45 +0800

    Update 四六级单词查询.py

--- 四六级单词查询.py
+++ 四六级单词查询.py
@@ -9,18 +9,13 @@ text_list = text.readlines()
 text_plain = re.sub(r'[{}]'.format(punctuation), '', ''.join(text_list))
 one_word_list = [word.lower() for word in text_plain.split()]
 print(one_word_list)#输出一个单词列表
-
-
-
 def run_query(wanted):
     word_total = 0
     # 统计出现的总个数
     for each in one_word_list:
         if each == wanted:
             word_total += 1
-
     print('"{}" occurs {} times'.format(wanted, word_total))
-
     line_number = 0
     for line in text_list:#循环遍历列表
         line_plain = re.sub(r'[{}]'.format(punctuation), '', line)
@@ -31,12 +26,9 @@ def run_query(wanted):
         if wanted in word_list:
             # 而下标"0"表示第一行，故需要减去1
             print('\tline {}: {}'.format(line_number, text_list[line_number - 1]), end='')
-
-
 if __name__ == '__main__':#调用主函数
     while True:
         sought = input('Input a word you want to search: ')
         if sought == 'q':
             break#跳出循环
-
         run_query(sought)
