862aeb9c930028c7626b10ae5541e6bbc689143b
PL96 <48303839+PL96@users.noreply.github.com>
2019-04-25 20:22:17 +0800

    杨普伦
    杨普伦

---
+++ 代码ypl.py
@@ -0,0 +1,12 @@
+# -*- coding: cp936 -*-
+import linecache
+o=open('单词表.csv')#打开文件
+o.seek(0)
+r=o.readlines()     #读取文件
+s1=str(input('输入要定位的单词信息:'))
+a=0
+count=0
+for i in r: #按和值组合查找
+    if i.find(s1)>=0:       
+        print(i)#显示定位行
+print ('Total:%d'%count)
---
+++ 单词查询ypl.py
@@ -0,0 +1,43 @@
+# 查询文本中某单词出现的次数，并打印其出现的行号及所在行的内容
+# 只适用于英文文本
+import re
+from string import punctuation
+
+text = open('find.txt')       #打开文件
+text_list = text.readlines()  #读起所有行
+# 删除标点及其他常用符号
+# punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+text_plain = re.sub(r'[{}]'.format(punctuation), '', ''.join(text_list))
+# 大写转换为小写，便于正确统计单词数
+one_word_list = [word.lower() for word in text_plain.split()]
+print(one_word_list)
+
+
+def run_query(wanted):     #定义一个函数
+    word_total = 0
+    # 统计出现的总个数
+    for each in one_word_list:   #如果搜索的单词在单词表里面
+        if each == wanted:
+            word_total += 1     #该词个数自加1
+
+    print('"{}" occurs {} times'.format(wanted, word_total))
+
+    line_number = 0
+    for line in text_list:
+        line_plain = re.sub(r'[{}]'.format(punctuation), '', line)
+        word_list = [word.lower() for word in line_plain.split()]
+        # 按照用户习惯第一行从"1"开始
+        line_number += 1
+        # 每行的单词列表
+        if wanted in word_list:
+            # 而下标"0"表示第一行，故需要减去1
+            print('\tline {}: {}'.format(line_number, text_list[line_number - 1]), end='')
+
+
+if __name__ == '__main__':     #定义一个函数
+    while True:
+        sought = input('Input a word you want to search: ')#输入要搜索的单词
+        if sought == 'q':  #如果找到
+            break          #跳出
+
+        run_query(sought)
