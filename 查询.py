000f13bf3c68839cb214797eb573fe927ac866f8
hewanpei <48540238+hewanpei@users.noreply.github.com>
2019-04-25 20:29:10 +0800

    Update 查询.py

--- 查询.py
+++ 查询.py
@@ -2,7 +2,6 @@
 # 只适用于英文文本
 import re
 from string import punctuation
-
 text = open('find.txt')
 text_list = text.readlines()
 # 删除标点及其他常用符号
@@ -11,18 +10,14 @@ text_plain = re.sub(r'[{}]'.format(punctuation), '', ''.join(text_list))
 # 大写转换为小写，便于正确统计单词数
 one_word_list = [word.lower() for word in text_plain.split()]
 print(one_word_list)
-
-
 def run_query(wanted):
     word_total = 0
     # 统计出现的总个数
     for each in one_word_list:
         if each == wanted:
             word_total += 1
-
-    print('"{}" occurs {} times'.format(wanted, word_total))
-
-    line_number = 0
+  print('"{}" occurs {} times'.format(wanted, word_total))
+   line_number = 0
     for line in text_list:
         line_plain = re.sub(r'[{}]'.format(punctuation), '', line)
         word_list = [word.lower() for word in line_plain.split()]
@@ -30,8 +25,6 @@ def run_query(wanted):
         line_number += 1     # 每行的单词列表
         if wanted in word_list:   # 而下标"0"表示第一行，故需要减去1
             print('\tline {}: {}'.format(line_number, text_list[line_number - 1]), end='')
-
-
 if __name__ == '__main__':#作用是控制这两种情况执行代码的过程，在if __name__ == 'main': 下的代码只有文件作为脚本直接执行才会被执行，而import到其他脚本中是不会被执行的。
     while True:
         sought = input('Input a word you want to search: ')
