6266519c0e4b9d98f4aa1b7a1e3f9742c16af00c
q1830067514 <48304521+q1830067514@users.noreply.github.com>
2019-04-25 21:24:12 +0800

    周丽玲 (#9)
    * Add files via upload
    
    * Add files via upload
    
    * 四六级单词查询
    
    * 删除ht.py
    
    * 修改
    
    * Delete 四级

---
+++ zll.py
@@ -0,0 +1,10 @@
+# -*- coding: cp936 -*-
+import linecache
+o=open('英语四六级.txt')#文件路径
+o.seek(0)
+r=o.readlines()
+s1=str(input('输入要定位的信息:'))
+for i in r: #按和值组合查找
+    if i.find(s1)>=0:       
+        print(i)#显示定位行
+print ('Total:%d'%count)
