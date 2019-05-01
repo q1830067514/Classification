0f603e59ed441eebf6a7c55170697df7d17326d7
fuyuanjie <48304385+fuyuanjie@users.noreply.github.com>
2019-05-01 22:53:55 +0800

    Update and rename 单词查询.py to 英语查询.py

--- 英语查询.py
+++ 英语查询.py
@@ -1,8 +1,5 @@
-# 查询文本中某单词出现的次数，并打印其出现的行号及所在行的内容
-# 只适用于英文文本
 import re
 from string import punctuation
-
 text = open('find.txt')
 text_list = text.readlines()
 # 删除标点及其他常用符号
@@ -11,17 +8,13 @@ text_plain = re.sub(r'[{}]'.format(punctuation), '', ''.join(text_list))
 # 大写转换为小写，便于正确统计单词数
 one_word_list = [word.lower() for word in text_plain.split()]
 print(one_word_list)
-
-
 def run_query(wanted):
     word_total = 0
     # 统计出现的总个数
     for each in one_word_list:
         if each == wanted:
             word_total += 1
-
-    print('"{}" occurs {} times'.format(wanted, word_total))
-
+            print('"{}" occurs {} times'.format(wanted, word_total))
     line_number = 0
     for line in text_list:
         line_plain = re.sub(r'[{}]'.format(punctuation), '', line)
@@ -32,8 +25,6 @@ def run_query(wanted):
         if wanted in word_list:
             # 而下标"0"表示第一行，故需要减去1
             print('\tline {}: {}'.format(line_number, text_list[line_number - 1]), end='')
-
-
 if __name__ == '__main__':
     while True:
         sought = input('Input a word you want to search: ')
